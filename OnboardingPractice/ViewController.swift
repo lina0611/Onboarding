e26de1e7d91241fc36d0f39a3742561de3079e3b
lina0611 <linagao0611@gmail.com>
2019-05-14 11:02:19 -0400

    first commit

---
+++ OnboardingPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OnboardingPractice
+//
+//  Created by Lina Gao on 5/14/19.
+//  Copyright © 2019 Lina Gao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
